f0374c51255067c7eef68e00bd08b7dc5fd6431f
Jmk125 <113652078+Jmk125@users.noreply.github.com>
2026-07-15 15:11:55 -0400

    Delete build_exe.bat

--- build_exe.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "%~dp0"
-python -m pip install -r requirements.txt
-python -m pip install pyinstaller
-python -m PyInstaller --noconfirm --clean --onefile --windowed --name UnitCostLookup unit_cost_lookup.py
-echo Built: dist\UnitCostLookup.exe
-pause
